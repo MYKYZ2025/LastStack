d83fce51700201809736ccfa9724400c2b892415
Mikolaj Krol <mikolajkrol_praktyki@op.pl>
2025-08-06 10:04:45 +0200

    CssAdded

--- assets/lib/alien-greeting.js
+++ assets/lib/alien-greeting.js
@@ -1,3 +1,15 @@
+import '../styles/alien-greeting.css';
+
 export default function(message, inPeace = false) {
+    if(!inPeace){
+        setTimeout(() => {
+            import('../styles/alien-greeting.css');
+        }, 4000);
+    }
+
+
+
+
+
     console.log(`${message}! ${inPeace ? '🕊️ We come in peace.' : '💥 Or else!'}`);
 }
